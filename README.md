7e6e93aea4ff80299c09e5854bcaeca0d238272c
Wellyngton <wellyngtonmb@gmail.com>
2022-06-12 23:03:08 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # FLASK-BOTO3-LOCALSTACK-S3
-Integração utilizando Flask, Boto3 e LocalStack, para realizar upload e download de arquivo no S3.
+## Integração utilizando Flask, Boto3 e LocalStack, para realizar upload e download de arquivo no S3.
+
+Código em python utilizando [Flask](https://flask.palletsprojects.com/en/2.1.x/) e [Boto3](https://pypi.org/project/boto3/) para fazer conexão com o [LocalStack S3](https://localstack.cloud/) e realizar o upload e download de arquivos através de requisições REST utilizando o [Postman](https://www.postman.com/).
+
+---
+### **Requisitos**
+- [Docker](https://www.docker.com/)
+- [Postman](https://www.postman.com/)
+---
+### **Inicializando**
+1. Clone o repositório
+2. Na raiz do repositório execute o comando `docker-compose up -d`
+3. Crie no localstacke o bucket `bucket-teste`
+4. Importe o arquivo `FLASK-BOTO3-LOCALSTACK-S3.postman_collection.json` no postman
+5. Realize a requisição mais adequada a sua situação no postman (Upload - file / Upload - file and json / Download - file)
+
+---
+### **Criando o bucket**
+O bucket pode ser criado por qualquer método preferível como:
+ - `awslocal`
+ - `awscli`
+ - `LocalStack Web App`
+ - `Ou por qualquer outro método.`
+
+Todas as informações sobre os métodos citados acima podem ser encontradas no próprio site do [LocalStack](https://localstack.cloud/).
+
+Caso queira criar o bucket com um nome diferente deve se modificar a linha 33 do arquivo `docker-compose.yml` para que fique com o mesmo nome do bucket que sera criado.
+
+---
+### **Variáveis de ambiente do docker-compose.yml**
+`AWS_ACCESS_KEY_ID` e `AWS_SECRET_ACCESS_KEY`  são obrigatórios pois o localstack valida se elas estão sendo passados, mas não valida os valoreis delas.
+
+Portanto pode se passar qualquer valor para essas duas variáveis que eles serão aceitos. 
+```
+AWS_ACCESS_KEY_ID: teste123
+AWS_SECRET_ACCESS_KEY: teste123
+```
+---
+
+`BUCKET_NAME` especifica o nome do bucket onde sera salvo o arquivo de upload e de onde sera realizado o download.
+
+Caso queira utilizar um bucket com nome diferente deve alterar o nome do buckets nesta variável.
+```
+BUCKET_NAME: bucket-teste
+```
+---
+`AWS_URL` especifica a url do localstack, como esta sendo passado através do docker-compose deve ser passado no formato `http:// + nome_do_container_do_localstack + :4566`.
+Caso seja passado fora do docker-compose deve ser passado o valor `http://localhost:4566`
+```
+AWS_URL: http://localstack:4566
+```
+---
+### **Requisições Postman**
+A collection postman disponibilizada possui 3 operações.
+- Upload - file
+- Upload - file and json
+- Download - file
+
+#### **POST Upload - file**
+Recebe como parâmetro somente o arquivo a ser enviado para o S3.
+![](./img/Postman-Upload-file.png)
+
+#### **POST Upload - file and json**
+Recebe como parâmetros o arquivo a ser enviado para o S3 e um json.
+![](./img/Postman-Upload-file-and-json.png)
+
+Para adicionar o campo `CONTENT TYPE` utilize os 3 pontos que fica ao lado de `DESCRIPTION`
+![](./img/Postman-Upload-file-and-json2.png)
+
+#### **GET Download - file**
+Recebe como path parâmetro o nome do arquivo a ser baixado do S3.
+
+Para utilizar a operação de download é necessário que o arquivo passado como paramento exista no S3.
+![](./img/Postman-Download-file.png)
---
+++ app/app.py
@@ -0,0 +1,74 @@
+from cgi import test
+import os
+import boto3
+import json
+from flask import Flask, request, jsonify, send_file
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'  # pasta onde será salvo o arquivo que será enviado para o S3
+DOWNLOAD_FOLDER = './downloads'  # pasta onde será salvo o arquivo que será baixado do S3
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}  # extensões permitidas
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')  # chave de acesso do AWS
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  # chave de secreta do AWS
+AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION') # região do AWS
+BUCKET_NAME = os.environ.get('BUCKET_NAME')  # nome do bucket
+AWS_URL = os.environ.get('AWS_URL')
+
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
+# verifica se o arquivo é permitido
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# upload do arquivo
+@app.route('/upload', methods=['POST'])
+def upload_file(): 
+    try:
+        if 'file' not in request.files:
+            return jsonify({'message': 'No file part in the request'}), 400
+        file = request.files['file']
+        if file.filename == '':            
+            return jsonify({'message': 'No file selected for uploading'}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename) # Craia um nome seguro para o arquivo
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Define o caminho que sera salvo o arquivo
+            file.save(file_path) # Salva o arquivo localmente
+            # Cria a conexão com o S3 (localstack ou aws))
+            s3 = boto3.client('s3', endpoint_url=AWS_URL, region_name=AWS_DEFAULT_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+            # Envia o arquivo para o S3
+            s3.upload_file(file_path, BUCKET_NAME, filename)
+            # Verifica se a key idKey é passada
+            if 'idKey' not in request.form:
+                return jsonify(message='File successfully uploaded'), 201
+
+            # Caso seja enviado do um json junto a requisição, deve ser feito passando através key 'idKey']
+            # Modifique o código abaixo para realizar as ações necessárias com o json enviado
+            teste_json = json.loads(request.form['idKey']) # Carrega o id do usuário
+            teste = teste_json['teste'] # Passa o valor teste para a variável teste
+            return jsonify(message='File successfully uploaded', teste=teste), 201
+        return jsonify(message='File not allowed'), 400
+    except:
+        # Caso ocorra algum erro, retorna o erro
+        return jsonify(message='Internal Server Error'), 500
+
+# download do arquivo
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    try:
+        #  file_path - Define o caminho que sera salvo o arquivo
+        file_path = app.config['DOWNLOAD_FOLDER'] + '/' + filename
+        # Cria a conexão com o S3 (localstack ou aws))
+        s3 = boto3.client('s3', endpoint_url=AWS_URL, region_name=AWS_DEFAULT_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        # Baixa o arquivo do S3
+        s3.download_file(BUCKET_NAME, filename, file_path)
+        # Retorna o arquivo baixado do S3 para o cliente
+        return send_file(file_path)
+    except:
+        # Retorna erro ao cliente
+        return jsonify(message='Internal Server Error'), 500
